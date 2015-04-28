2a44f4132dd338806e460d9f6d354c8ad3335515
Mehrdad Yazdani <myazdani@gmail.com>
2015-04-28 14:27:00 -0700

    corrected OpenCV path

--- face_and_featureExtraction.py
+++ face_and_featureExtraction.py
@@ -4,17 +4,17 @@ import csv
 from joblib import Parallel, delayed
 from time import time
 import sys
-import pickle
+import cPickle as pickle
 import json
 from numpy import *
 #src_path = sys.argv[1]
 #classifier_path = sys.argv[2]
 #write_path = sys.argv[3]
-src_path = "/Users/myazdaniUCSD/Documents/selfiecity_v1/images/selfies/thumb/"
-classifier_path = '/Applications/OpenCV/opencv-2.4.8/data/haarcascades/haarcascade_frontalface_alt.xml'
-write_path = "./results"
+src_path = "/data/myazdani/images/2013"
+classifier_path = '/home/fiona/Dev/opencv-master/data/haarcascades/haarcascade_frontalface_alt_tree.xml'
+write_path = "./"
 
-image_paths_list = []  
+image_paths_list = []
 for root, dirs, files in os.walk(src_path):
   temp = [os.path.join(root, f) for f in files if f.endswith('.jpg')]
   if len(temp) > 0: image_paths_list.extend(temp)
@@ -22,7 +22,7 @@ for root, dirs, files in os.walk(src_path):
 def chunks(l, n):
   for i in xrange(0, len(l), n): yield l[i:i+n]
 
-image_paths_list = list(chunks(image_paths_list, len(image_paths_list)/4))
+image_paths_list = list(chunks(image_paths_list, len(image_paths_list)/1))
 
 face_cascade = cv2.CascadeClassifier(classifier_path)
 #face_cascade = cv2.CascadeClassifier()
@@ -41,17 +41,18 @@ def return_face_dict(path):
     face_dict['file_path'] = path
     face_dict['num_faces'] = len(faces)
     face_dict['face_boxes'] = faces
+
     if len(faces) > 0:
       rgb_hist = []
       hsv_hist = []
       #rois = []
       for face in faces:
-        roi = img[face[1]:face[1]+face[3], face[0]:face[0]+face[2],]    
+        roi = img[face[1]:face[1]+face[3], face[0]:face[0]+face[2],]
         #rois.append(roi)
         rgb_hist.append([cv2.calcHist([roi],[i],None,[256],[0,256]) for i in range(3)])
         roi_HSV = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
         for channel in range(3):
-          if channel == 0: 
+          if channel == 0:
             hsv_hist.append(cv2.calcHist([roi_HSV],[channel],None,[180],[0,180]))
           else:
             hsv_hist.append(cv2.calcHist([roi_HSV],[channel],None,[256],[0,256]))
@@ -65,8 +66,8 @@ t0 = time()
 
 for i, image_paths in enumerate(image_paths_list):
   print "working on batch", str(i+1), "out of", str(len(image_paths_list))
-  results= Parallel(n_jobs=4)(delayed(return_face_dict)(image_path) for image_path in image_paths)
-  with open(write_path + '/results_dict_' + str(i) + '.pkl', 'wb') as output: pickle.dump(results, output, pickle.HIGHEST_PROTOCOL)
+  results= Parallel(n_jobs=-1)(delayed(return_face_dict)(image_path) for image_path in image_paths)
+  with open(write_path + '/results_alt_tree_images_dict_' + str(i) + '.pkl', 'wb') as output: pickle.dump(results, output, pickle.HIGHEST_PROTOCOL)
 
   #output = [[result['file_path'], result['num_faces']] for result in results]
 '''
@@ -74,6 +75,5 @@ for i, image_paths in enumerate(image_paths_list):
     writer = csv.writer(f, delimiter = ",")
     writer.writerow(['file_path', 'num_faces'])
     for item in output: writer.writerow([item[0], item[1]])
-'''  
+'''
 print time() - t0
-
